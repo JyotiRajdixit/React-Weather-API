602e78e00602975d12a59a7ee0681cad5aa1c83e
JyotiRajdixit <47452906+JyotiRajdixit@users.noreply.github.com>
2020-08-01 22:49:50 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -39,14 +39,9 @@ class App extends React.Component {
       boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
       padding: "5px",
     }
-    if(this.state.isLoading) {
-      return <h1>Loading...!</h1>
-    }
-    if (!this.state.details) {
-       return <h2>No such city</h2>
-     }
     return (
       <div>
+        {this.state.isLoading ? <h1>Loading...!</h1> :
         <center>
          <div style={mystyle}>
           <h1>
@@ -60,7 +55,7 @@ class App extends React.Component {
            {this.state.details.weather[0].description}
           </h3>
          </div>
-        </center>
+        </center>}
       </div>
     )
   }
