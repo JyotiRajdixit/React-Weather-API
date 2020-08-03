0be7a539a671e86dd7891aed6b492f7919e5879b
JyotiRajdixit <47452906+JyotiRajdixit@users.noreply.github.com>
2020-08-03 23:54:25 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       isLoading: true,
       details: null,
